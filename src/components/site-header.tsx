994a0f58a27d8cb09ada052ea988602fe43db48d
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-06-02 00:31:18 +0200

    Change header height

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -7,7 +7,7 @@ import { Company } from "@/components/company";
 export default function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center">
+      <div className="container flex h-20 max-w-screen-2xl items-center">
         <Company />
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <MainNav />
