d1e6ff1a3e4d52d159bc356b1d1d1a248ed4f49b
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-06-03 23:47:58 +0200

    clean code

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -3,11 +3,6 @@
 import * as React from "react";
 import { useTheme } from "next-themes";
 import { DarkLogo, LightLogo } from "@/assets/icons/logo";
-
-interface LogoProps {
-  className?: string;
-}
-
 export function Logo() {
   const { theme } = useTheme();
 
