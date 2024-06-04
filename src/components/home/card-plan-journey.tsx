1a75d227e2305a5ee21f8e1d2fe5f7de94af068e
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-06-04 10:00:12 +0200

    adjust help button

--- src/components/home/card-plan-journey.tsx
+++ src/components/home/card-plan-journey.tsx
@@ -105,7 +105,8 @@ export function CardPlanJourney() {
             <div className="lg:pl-2 pl-1">Letzte Reisen</div>
           </TabsTrigger>
         </TabsList>
-        <Button className="mr-4 bg-indigo-900 hover:bg-indigo-800 dark:text-zinc-100">
+        <Button className="mr-4 bg-zinc-700 hover:bg-zinc-600 dark:text-zinc-100">
+          {/*<Button className="mr-4 bg-indigo-900 hover:bg-indigo-800 dark:text-zinc-100">*/}
           <MessageCircleQuestion className="mr-2 h-4 w-4" />
           Brauchst du Hilfe?
         </Button>
