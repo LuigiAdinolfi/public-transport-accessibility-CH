5e967d9ca709f82bc62e0a494896ad5eda57ccf1
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-06-09 00:17:10 +0200

    update rating stars

--- src/components/plan/rating-stars.tsx
+++ src/components/plan/rating-stars.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { useState } from "react";
-import { Rating as ReactRating } from "@smastrom/react-rating";
+import { Rating, Star } from "@smastrom/react-rating";
 
 export function RatingStars() {
   const [rating, setRating] = useState(0);
 
+  const myStyles = {
+    itemShapes: Star,
+    itemStrokeWidth: 2,
+    activeFillColor: "#A1A1AA",
+    activeStrokeColor: "#18181B",
+    inactiveFillColor: "#FAFAFA",
+    inactiveStrokeColor: "#A1A1AA",
+  };
+
   return (
-    <ReactRating
+    <Rating
       style={{ maxWidth: 120 }}
       value={rating}
       onChange={setRating}
+      itemStyles={myStyles}
     />
   );
 }
