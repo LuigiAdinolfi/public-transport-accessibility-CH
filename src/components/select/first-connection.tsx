046b264ed9dea29421ce27958335d7d1128d187f
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-06-17 11:32:53 +0200

    update next & react versions

--- src/components/select/first-connection.tsx
+++ src/components/select/first-connection.tsx
@@ -1,12 +1,12 @@
-import { Card, CardContent } from "@/components/ui/card";
-
 export function FirstConnection() {
   return (
-    <Card className="flex basis-1/2 justify-start border-opacity-0 bg-zinc-50">
-      <CardContent className="flex w-full justify-between px-3 py-2">
-        <div className="flex justify-start">20:28</div>
-        <div className="flex justify-end">20:55</div>
-      </CardContent>
-    </Card>
+    <div className="flex basis-1/2 justify-start rounded-lg bg-zinc-50">
+      <div className="grid w-full grid-flow-col grid-rows-4">
+        <div className="flex w-full justify-between px-3 py-2">
+          <div className="flex justify-start">20:28</div>
+          <div className="flex justify-end">20:55</div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/select/last-connection.tsx
+++ src/components/select/last-connection.tsx
@@ -1,12 +1,12 @@
-import { Card, CardContent } from "@/components/ui/card";
-
 export function LastConnection() {
   return (
-    <Card className="flex basis-1/2 justify-start border-opacity-0 bg-zinc-50">
-      <CardContent className="flex w-full justify-between px-3 py-2">
-        <div className="flex justify-start">21:02</div>
-        <div className="flex justify-end">21:28</div>
-      </CardContent>
-    </Card>
+    <div className="flex basis-1/2 justify-start rounded-lg bg-zinc-50">
+      <div className="grid w-full grid-flow-col grid-rows-4">
+        <div className="flex w-full justify-between px-3 py-2">
+          <div className="flex justify-start">21:02</div>
+          <div className="flex justify-end">21:28</div>
+        </div>
+      </div>
+    </div>
   );
 }
