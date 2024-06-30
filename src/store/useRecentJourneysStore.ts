854cf9125ebdfadec34c40e2007736553987d405
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-06-30 15:56:01 +0200

    fix localStorage minor bug

--- src/store/useRecentJourneysStore.ts
+++ src/store/useRecentJourneysStore.ts
@@ -19,27 +19,24 @@ interface JourneyState {
 
 export const useRecentJourneysStore = create<JourneyState>((set, get) => {
   // Initialize recent journeys from localStorage or empty array
-  const initialRecentJourneys: RecentJourney[] = JSON.parse(
-    localStorage.getItem("recentJourneys") || "[]",
-  );
+  const initialRecentJourneys: RecentJourney[] =
+    typeof window !== "undefined" && localStorage.getItem("recentJourneys")
+      ? JSON.parse(localStorage.getItem("recentJourneys") || "[]")
+      : [];
+
   return {
     recentJourneys: initialRecentJourneys,
     addRecentJourney: (journey) =>
       set((state) => {
-        const updatedJourneys = [journey, ...state.recentJourneys];
-        localStorage.setItem("recentJourneys", JSON.stringify(updatedJourneys));
+        const updatedJourneys = [journey, ...state.recentJourneys].slice(0, 5); // Keep only the 5 most recent journeys
+        if (typeof window !== "undefined") {
+          localStorage.setItem(
+            "recentJourneys",
+            JSON.stringify(updatedJourneys),
+          );
+        }
         return { recentJourneys: updatedJourneys };
       }),
     getRecentJourneys: () => get().recentJourneys,
   };
 });
-
-// Without local storage
-// export const useRecentJourneysStore = create<JourneyState>((set, get) => ({
-//   recentJourneys: [],
-//   addRecentJourney: (journey) =>
-//     set((state) => ({
-//       recentJourneys: [journey, ...state.recentJourneys].slice(0, 5), // Keep only the 5 most recent journeys
-//     })),
-//   getRecentJourneys: () => get().recentJourneys,
-// }));
