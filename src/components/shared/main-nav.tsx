653d8621df6e5bd878385d903836b1f863d0bf45
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-07-05 15:11:12 +0200

    update header style

--- src/components/shared/main-nav.tsx
+++ src/components/shared/main-nav.tsx
@@ -30,9 +30,7 @@ export function MainNav(): React.ReactElement {
     if (resolvedTheme === "dark") {
       return cn(
         "transition-colors text-white",
-        isActive
-          ? "bg-zinc-700 dark:zinc-700 text-white dark:text-white font-semibold"
-          : "text-white hover:bg-zinc-800 hover:text-white font-semibold",
+        isActive ? "text-zinc-50 font-semibold" : "text-zinc-400 font-semibold",
         "text-lg",
         "rounded-md p-2",
         "px-6 py-2.5",
@@ -42,8 +40,8 @@ export function MainNav(): React.ReactElement {
       return cn(
         "transition-colors text-zinc-950",
         isActive
-          ? "bg-zinc-200 text-zinc-950 font-semibold dark:bg-zinc-700 dark:text-white"
-          : "text-zinc-950 dark:text-white hover:bg-zinc-100 hover:text-zinc-950 font-semibold dark:hover:bg-zinc-800",
+          ? "text-zinc-950 font-semibold"
+          : "text-zinc-600 font-semibold",
         "text-lg",
         "rounded-md p-2",
         "px-6 py-2.5",
