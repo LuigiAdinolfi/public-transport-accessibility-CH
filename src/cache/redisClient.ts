2df63f80f7bce274d1f2cad7c06dba2c4c8be9d1
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-07-12 12:39:53 +0200

    update redis variable names

--- src/cache/redisClient.ts
+++ src/cache/redisClient.ts
@@ -3,9 +3,9 @@ import Redis from "ioredis";
 // Initialize Redis client using the provided URL from environment variables
 const redis: Redis = new Redis({
   port: 12441, // Redis port
-  host: process.env.REDIS_HOST,
+  host: process.env.NEXT_PUBLIC_REDIS_HOST,
   username: "default",
-  password: process.env.REDIS_PASSWORD,
+  password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
 });
 
 // Log a message when successfully connected to Redis
