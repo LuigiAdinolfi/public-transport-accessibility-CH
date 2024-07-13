a6454f1a45adf2355bcb310f88d5ecfb880241c2
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2024-07-13 17:17:35 +0200

    Revert "update font-weight header"
    This reverts commit b153ef234ca36319d484f472d04d2eb4909c6ac3

--- src/components/shared/main-nav.tsx
+++ src/components/shared/main-nav.tsx
@@ -30,7 +30,7 @@ export function MainNav(): React.ReactElement {
     if (resolvedTheme === "dark") {
       return cn(
         "transition-colors text-white",
-        isActive ? "text-zinc-50 font-semibold" : "text-zinc-400",
+        isActive ? "text-zinc-50 font-semibold" : "text-zinc-400 font-semibold",
         "text-lg",
         "rounded-md p-2",
         "px-6 py-2.5",
@@ -39,7 +39,9 @@ export function MainNav(): React.ReactElement {
     } else {
       return cn(
         "transition-colors text-zinc-950",
-        isActive ? "text-zinc-950 font-semibold" : "text-zinc-600",
+        isActive
+          ? "text-zinc-950 font-semibold"
+          : "text-zinc-600 font-semibold",
         "text-lg",
         "rounded-md p-2",
         "px-6 py-2.5",
